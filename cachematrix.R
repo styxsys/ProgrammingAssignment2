db3c373aa796cde89ef5adf15c9cc0f7479f97c6
styxsys <969481221@qq.com>
2014-09-18 19:30:45 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The function "makeCacheMatrix" create a factor has 2 matrixes and 4
+## functions
 
-## Write a short comment describing this function
+## The matrix "x" is the original arguement matrix while matrix "inv" is the
+## inverse matrix of x
+
+## The functions aim to get and set values of the 2 matrixes
+
+## Function "cacheSolve" calculates "inv" for the factor created by
+## "makeCacheMatrix"
+
+## This function creates a special "matrix", which is really a list containing
+## a function to:
+## 1.set the value of the matrix
+## 2.get the value of the matrix
+## 3.set the value of the inverse matrix
+## 4.get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv<-NULL
+	set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## If the factor already has the value of "inv", it gets the inverse matrix
+## from the cache and skips the computation
+## Otherwise, it calculates the inverse matrix of "data" and sets the value of
+## the inverse matrix in the cache via the setinv function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	 inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
